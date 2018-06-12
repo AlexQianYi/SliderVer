aa0c7b59121b8dd89407e27b564853be381781bd
AlexQianYi <q563875735@gmail.com>
2018-06-12 14:16:36 +0800

    download images

--- SpiderImg.py
+++ SpiderImg.py
@@ -3,30 +3,62 @@
 import requests
 from selenium import webdriver
 import time as t
+import utility
+import urllib
 from selenium.webdriver.support import expected_conditions as Expect
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 
-import utility
-
-# wangyi url
-url = 'https://id.163yun.com/login?referrer=https://dun.163.com/dashboard&h=yd&fromyd=baiduP2_YZM_CP1934'
-
-# bg img xpath
-bg_xpath = '//*[@id="bg"]/div[2]/div/div/div/div/div[1]/form/div[3]/div/div/div[1]/div/div[1]/img[1]'
-# slider img xpath
-slider_xpath = '//*[@id="bg"]/div[2]/div/div/div/div/div[1]/form/div[3]/div/div/div[1]/div/div[1]/img[2]'
-
-# launch firefox
-dr = webdriver.Firefox(executable_path='/Users/yiqian/Downloads/geckodriver')
-
-# record the image have loaded
 bg_img_dic = {}
-slider_ximg_dic = {}
+slider_img_dic = {}
 
-# start firefox
-dr.get(url)
-t.sleep(5)
+def get_bg_slider_img(m):
+
+    # bg img xpath
+    bg_xpath = '//*[@id="bg"]/div[2]/div/div/div/div/div[1]/form/div[3]/div/div/div[1]/div/div[1]/img[1]'
+    # slider img xpath
+    slider_xpath = '//*[@id="bg"]/div[2]/div/div/div/div/div[1]/form/div[3]/div/div/div[1]/div/div[1]/img[2]'
+
+    # launch firefox
+    dr = webdriver.Firefox(executable_path=utility.firefox_driver_path)
+
+    # record the image have loaded
+    global bg_img_dic, slider_img_dic
+
+    # start firefox
+    dr.get(utility.url)
+    t.sleep(5)
+
+    bg_img_element = dr.find_element_by_xpath(bg_xpath)
+    slider_img_element = dr.find_element_by_xpath(slider_xpath)
+
+    bg_img_url = bg_img_element.get_attribute('src')
+    slider_img_url = slider_img_element.get_attribute('src')
+
+    if bg_img_url != None and not bg_img_url in bg_img_dic:
+
+        bg_img_dic[bg_img_url] = ''
+        slider_img_dic[slider_img_url] = ''
+
+        bg_img_ext = bg_img_url.split('.')[-1]                      # .jpg
+        slider_img_ext = slider_img_url.split('.')[-1]              # .png
+
+        bg_file_name = str(m) + '.' + bg_img_ext
+        slider_file_name = str(m) + '.' + slider_img_ext
+
+        # save image file
+        bg_file_data = urllib.
+
+        print(bg_img_ext)
+        print(slider_img_ext)
+
+
+if __name__ == "__main__":
+
+    get_bg_slider_img(1)
+
+
+"""
 
 # WebDriverWait(dr, 5).until(Expect.text_to_be_present_in_element((By.CSS_SELECTOR,"#TANGRAM__PSP_8__error"),u'请您填写手机/邮箱/用户名'))
 
@@ -43,11 +75,9 @@ dr.close()
 
 
 
-"""
 email = dr.find_element_by_css_selector("div[data-target='account-login']")
 email.click()
-"""
-"""
+
 PhoneNumberInput = dr.find_element_by_name("phoneNumberInput")
 PhoneNumberInput.send_keys(utility.PhoneNumer)
 
@@ -64,14 +94,11 @@ register.click()
 
 offset = get_gap_offset(browser)
 drag_and_drop(dr, offset)
-"""
 
 
 print('Browser will be closed')
 
-"""
 download dataset
-"""
 
 def downimage(i, FileDes):
     # 构建session
@@ -92,3 +119,5 @@ if __name__ == "__main__":
     for i in range(1000):
         print('正在下载: ',i+1)
         downimage(i, "./VerPic/" + str(i) + "image.jpg")
+        
+"""
--- utility.py
+++ utility.py
@@ -1,3 +1,15 @@
 
 
 PhoneNumer = '13899592637'
+
+# wangyi url
+url = 'https://id.163yun.com/login?referrer=https://dun.163.com/dashboard&h=yd&fromyd=baiduP2_YZM_CP1934'
+
+#firefox driver path
+firefox_driver_path = '/Users/yiqian/Downloads/geckodriver'
+
+# bg image save path
+bg_img_file = './BgImg'
+
+# slider image save path
+slider_img_file = './SliderImg'
