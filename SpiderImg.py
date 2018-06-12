2994e0d61153385cc8186e6d70ba88cdbfe90749
AlexQianYi <q563875735@gmail.com>
2018-06-12 11:23:27 +0800

    spider bg/slider img

--- SpiderImg.py
+++ SpiderImg.py
@@ -1,3 +1,6 @@
+# coding:utf8
+
+import requests
 from selenium import webdriver
 import time as t
 from selenium.webdriver.support import expected_conditions as Expect
@@ -6,10 +9,25 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 import utility
 
+# wangyi url
+url = 'https://id.163yun.com/login?referrer=https://dun.163.com/dashboard&h=yd&fromyd=baiduP2_YZM_CP1934'
+
+# bg img xpath
+bg_xpath = '//*[@id="bg"]/div[2]/div/div/div/div/div[1]/form/div[3]/div/div/div[1]/div/div[1]/img[1]'
+# slider img xpath
+slider_xpath = '//*[@id="bg"]/div[2]/div/div/div/div/div[1]/form/div[3]/div/div/div[1]/div/div[1]/img[2]'
+
+# launch firefox
 dr = webdriver.Firefox(executable_path='/Users/yiqian/Downloads/geckodriver')
 
-dr.get('https://id.163yun.com/login?referrer=https://dun.163.com/dashboard&h=yd&fromyd=baiduP2_YZM_CP1934')
+# record the image have loaded
+bg_img_dic = {}
+slider_ximg_dic = {}
+
+# start firefox
+dr.get(url)
 t.sleep(5)
+
 # WebDriverWait(dr, 5).until(Expect.text_to_be_present_in_element((By.CSS_SELECTOR,"#TANGRAM__PSP_8__error"),u'请您填写手机/邮箱/用户名'))
 
 print('start find')
@@ -23,7 +41,7 @@ print('click finish')
 
 dr.close()
 
-//*[@id="bg"]/div[2]/div/div/div/div/div[1]/form/div[3]/div/div/div[1]/div/div[1]/img[1]
+
 
 """
 email = dr.find_element_by_css_selector("div[data-target='account-login']")
@@ -50,3 +68,27 @@ drag_and_drop(dr, offset)
 
 
 print('Browser will be closed')
+
+"""
+download dataset
+"""
+
+def downimage(i, FileDes):
+    # 构建session
+    sess = requests.Session()
+    # 建立请求头
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36",
+        "Connection": "keep-alive"}
+    url = "https://account.flycua.com/sso/chineseVerifyCode.images"
+    # 获取响应图片内容
+    image = sess.get(url, headers=headers).content
+    # 保存到本地
+    with open(FileDes, "wb") as f:
+        f.write(image)
+
+
+if __name__ == "__main__":
+    for i in range(1000):
+        print('正在下载: ',i+1)
+        downimage(i, "./VerPic/" + str(i) + "image.jpg")
